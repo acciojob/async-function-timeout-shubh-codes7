de48cbebb1aa3d04c5c0be42746aa0388ac473d9
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-02 13:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let btn = document.querySelector("#btn")
+let output = document.querySelector("#output")
+
+function delayMsg(ms){
+	return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+btn.addEventListener("click", async ()=>{
+	let msg = document.querySelector("#text").value
+	let delay = parseInt(document.querySelector("#delay").value)
+	
+	await delayMsg(delay)
+	output.textContent = msg;
+})
